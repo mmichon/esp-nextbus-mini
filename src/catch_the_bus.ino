d0fe7e87fe1f028eb20d47716fbf2a3d4b4008f8
Michael Michon <mmichon@gmail.com>
2018-05-30 19:24:50 -0700

    Tidy code formatting and switch to platformio.ini-based lib management

--- src/catch_the_bus.ino
+++ src/catch_the_bus.ino
@@ -1,46 +1,59 @@
+#include "config_mike.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <SPI.h>
+#include <WiFiManager.h>
 #include <Wire.h>
-#include <Adafruit_SSD1306.h>   // git clone https://github.com/mcauser/Adafruit_SSD1306/tree/esp8266-64x48 lib
-#include <Adafruit_GFX.h>       // pio lib install 13
-#include <WiFiManager.h>        // pio lib install 567
-#include <tinyxml2.h>           // git clone https://github.com/leethomason/tinyxml2 lib
-#include "config.h"
+#include <tinyxml2.h>
 
-
-#define OLED_RESET 0  // GPIO0
+#define OLED_RESET 0 // GPIO0
 Adafruit_SSD1306 display(OLED_RESET);
 #if (SSD1306_LCDHEIGHT != 48)
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
 
-
 WiFiManager wifiManager;
 WiFiClient client;
 
-
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(115200);
+
+    Serial.println("Connecting to display...");
 
     // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
-    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 64x48)
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // initialize with the I2C addr 0x3C (for the 64x48)
+    clear_display();
+
+    Serial.println("Connecting to wifi...");
+
+    //wait up to 3 minutes to connect to AP
+    wifiManager.setTimeout(180);
 
     // configure wifi on first connect otherwise just connect to the last known
-    wifiManager.autoConnect();
+    if (!wifiManager.autoConnect()) {
+        Serial.println("Failed to connect and hit timeout; resetting");
+        delay(3000);
 
+        ESP.reset();
+        delay(5000);
+    }
+
+    Serial.println("Downloading prediction...");
     // download the predictions once we're connected
     String prediction_xml = download_prediction_xml();
-    const char* xml = prediction_xml.c_str();
+    const char *xml = prediction_xml.c_str();
 
+    Serial.println("Parsing prediction...");
     // let's parse out a couple predictions!
-    if(strcmp(xml, "ERROR") != 0) {
+    if (strcmp(xml, "ERROR") != 0) {
         int first_prediction, second_prediction;
         get_prediction(xml, first_prediction, second_prediction);
 
+        Serial.println("Displaying prediction...");
         display_predictions(first_prediction, second_prediction);
-    }
-    else {
+    } else {
         display_error();
     }
 
@@ -55,33 +68,33 @@ void loop() {
 }
 
 // clears the display and sets basic settings
-void clear_display(){
+void clear_display() {
     display.clearDisplay();
     display.setTextColor(WHITE);
-    display.setCursor(0,0);
+    display.setCursor(0, 0);
 }
 
 // displays predictions on the display
 void display_predictions(int first_prediction, int second_prediction) {
-        clear_display();
+    clear_display();
 
-        display.setTextSize(1);
-        display.println("Next bus:");
-        display.println();
+    display.setTextSize(1);
+    display.println("Next bus:");
+    display.println();
 
-        display.setTextSize(3);
-        display.print(first_prediction);
-        display.setTextSize(2);
-        display.print(second_prediction);
-        display.setTextSize(3);
-        display.println();
+    display.setTextSize(3);
+    display.print(first_prediction);
+    display.setTextSize(2);
+    display.print(second_prediction);
+    display.setTextSize(3);
+    display.println();
 
-        display.setTextSize(1);
-        display.println("minutes");
+    display.setTextSize(1);
+    display.println("minutes");
 }
 
 // display error in case of problems getting predictions
-void display_error(){
+void display_error() {
     clear_display();
 
     display.setTextSize(2);
@@ -102,7 +115,7 @@ String download_prediction_xml() {
     }
 
     String url = String("/service/publicXMLFeed?command=predictions&a=") + nb_agency +
-        "&r=" + nb_route + "&s=" + nb_stop;
+                 "&r=" + nb_route + "&s=" + nb_stop;
 
     Serial.print("Requesting URL: ");
     Serial.println(url);
@@ -112,15 +125,16 @@ String download_prediction_xml() {
                  "Connection: close\r\n\r\n");
     unsigned long timeout = millis();
     while (client.available() == 0) {
-      if (millis() - timeout > 5000) {
-        Serial.println(">>> Client Timeout !");
-        client.stop();
-        return String("ERROR");
-      }
+        if (millis() - timeout > 5000) {
+            Serial.println("Client timeout!");
+            client.stop();
+
+            return String("ERROR");
+        }
     }
 
     String response;
-    while(client.available()){
+    while (client.available()) {
         response += client.readStringUntil('\r');
     }
     //Serial.println("Response:"+response);
@@ -130,21 +144,20 @@ String download_prediction_xml() {
     //Serial.println("XML:" + xml_response);
     //Serial.println();
 
-    Serial.println("closing connection");
+    Serial.println("Closing connection.");
 
     return xml_response;
 }
 
 // extracts the first minutes from the prediction XML
 // check out https://gist.github.com/grantland/7cf4097dd9cdf0dfed14 for more info on the API
-void get_prediction(const char* xml, int &first_prediction, int &second_prediction) {
-    //Serial.println("Parsing XML: ");
-    //Serial.println(xml);
+void get_prediction(const char *xml, int &first_prediction, int &second_prediction) {
+    Serial.println("Parsing XML...");
 
     tinyxml2::XMLDocument doc;
     doc.Parse(xml);
 
-    tinyxml2::XMLElement* predictionsElement = doc.FirstChildElement("body")->FirstChildElement("predictions")->FirstChildElement("direction")->FirstChildElement("prediction");
+    tinyxml2::XMLElement *predictionsElement = doc.FirstChildElement("body")->FirstChildElement("predictions")->FirstChildElement("direction")->FirstChildElement("prediction");
     first_prediction = atoi(predictionsElement->Attribute("minutes"));
 
     // don't even bother with the first prediction if you can't walk to the stop fast enough
--- src/config.h
+++ src/config.h
@@ -2,10 +2,10 @@
 // you may have to remove the leading '1' from the stop number
 // for example, the below info comes from
 // https://www.nextbus.com/#!/sf-muni/KT/KT___O_P00/7355/5237
-const char* host = "webservices.nextbus.com";
-const char* nb_agency = "sf-muni";
-const char* nb_route = "KT";
-const char* nb_stop = "7355";
+const char *host = "webservices.nextbus.com";
+const char *nb_agency = "sf-muni";
+const char *nb_route = "KT";
+const char *nb_stop = "7355";
 
 // minimum time to walk to stop
 const int MIN_WALKING_TIME = 3;
